43a5a575d6a14319537ce2e0c4b98184b7126e7f
MiYaku Gaming <MiYakuT@yandex.com>
2024-12-06 20:20:03 -0600

    Update _build_patch.sh
    Updated per eadmaster.

--- src/buildcd/_build_patch.sh
+++ src/buildcd/_build_patch.sh
@@ -4,38 +4,37 @@
 # Made by eadmaster for the Princess Crown Translation GPL Edition project  https://github.com/eadmaster/pcrown/
 
 # Requirements:
-# - apt install iat  p7zip-full  xdelta3  wine  python3
+# - apt install iat  p7zip-full  xdelta3  wine  python3  xxd
 # - cd-replace (manual install) https://github.com/mistydemeo/cd-replace
 # - ucon64 (manual install)  https://ucon64.sourceforge.io/
 # - skf (manual install)  https://sourceforge.net/projects/swissfileknife/
 # - eventeditor.exe -> build via ../eventeditor/_build.sh
 # - itemsutil.exe -> build via ../itemsutil/_build.sh
 
+export CD_PATH=../../cd
+export TRANSLATED_SCRIPT_PATH=../../script/eng
+export SIGNS_PATH=../../signs
+
 # cleanup
 rm *.eng
 rm "Princess Crown (Japan) (1M) (Track 01) (English).iso"
-rm "Princess Crown (Japan) (1M) (Track 01) (English).iso.bak"
+rm "Princess Crown (Japan) (1M) (Track 01).iso"
 rm "Princess Crown (Japan) (1M) (Track 01) (English).iso.xdelta"
+rm "Princess Crown (Japan) (1M) (Track 01) (English).bin.xdelta"
 rm KANJI_ENG.BIN
 
 # convert data bin track to iso
-iat "Princess Crown (Japan) (1M) (Track 01).bin" "Princess Crown (Japan) (1M) (Track 01) (English).iso"
-cp "Princess Crown (Japan) (1M) (Track 01) (English).iso" "Princess Crown (Japan) (1M) (Track 01) (English).iso.bak"
+iat "Princess Crown (Japan) (1M) (Track 01).bin" "Princess Crown (Japan) (1M) (Track 01).iso"
+cp "Princess Crown (Japan) (1M) (Track 01).iso" "Princess Crown (Japan) (1M) (Track 01) (English).iso" 
 
-# update font
-7z e -y "Princess Crown (Japan) (1M) (Track 01) (English).iso" KANJI.BIN
+# update the font
+7z e -y "Princess Crown (Japan) (1M) (Track 01).iso" KANJI.BIN
 xdelta3 -f -d -s KANJI.BIN KANJI.BIN.xdelta KANJI_ENG.BIN  # apply English font patch
 #xdelta3 -e -s KANJI.BIN  KANJI_ENG.BIN  KANJI.BIN.xdelta  # create new font patch
 
-TRANSLATED_SCRIPT_PATH=../../script/eng
-# export CD_PATH=../../cd
-
 # update items and names
-7z e -y "Princess Crown (Japan) (1M) (Track 01) (English).iso" 0.BIN
+7z e -y "Princess Crown (Japan) (1M) (Track 01).iso" 0.BIN
 wine itemsutil.exe -i ${TRANSLATED_SCRIPT_PATH}/names.txt ${TRANSLATED_SCRIPT_PATH}/items.txt  0.BIN  KANJI_ENG.BIN  0xEA0   # 0xEA0 = starting write offset in KANJI_ENG.BIN, ranges are in itemsutils/main.cpp
-
-# enable debug mode (press Start on 2nd pad to navigate event files) https://web.archive.org/web/20200918203538/https://github.com/cyberwarriorx/pcrown/wiki/Debugging
-ucon64 --nbak --poke=1EB7F:01 0.BIN
 
 # make chars spacing smaller (thanks to paul_met and derek (ateam) for the tips) https://segaxtreme.net/threads/help-me-translate-princess-crown.18555/#post-186226
 #060729A8    E204
@@ -88,76 +87,136 @@ ucon64 --nbak --poke=3BA61:B8 0.BIN
 # Clearing the description window
 ucon64 --nbak --poke=47773:05 0.BIN
 ucon64 --nbak --poke=4777B:17 0.BIN
-# cook menu fix
-#05E6D56B-    FFEAFFFFEB00FFF3F700FFF7CFFFE9FFE8FF (old value)
-#05E6D57C     FFEAFFFFF7FFE8FFFFE9FFF7FFFFFFFFFFFF (new value)
-#05E6D57D-    FFEAFFEBFF00FFF7003CFFECC6FFFFE8FFF7FFFFE9 (old value)
-#05E6D591     FFEB00FFFFF7FFECC6FFFFEB04FFFFE8FFF7FFFFE9 (new value)
-#05E6D592-    FFEAFFFFEB00FFF7CF00FFECC63FFFE9FFE8FF  (old value)
-#05E6D5A4     FFEAFFFFF7FFECC6FFFFEB05FFFFE8FFF7FFFFF9 (new value)
-sfk replace KANJI_ENG.BIN -binary /FFEAFFFFEB00FFF3F700FFF7CFFFE9FFE8FF/FFEAFFFFF7FFE8FFFFE9FFF7FFFFFFFFFFFF/  -yes
-sfk replace KANJI_ENG.BIN -binary /FFEAFFEBFF00FFF7003CFFECC6FFFFE8FFF7FFFFE9/FFEB00FFFFF7FFECC6FFFFEB04FFFFE8FFF7FFFFE9/  -yes
-#NO? optional (for selling items - adding descriptions line for items). -> breaks the game
-#sfk replace KANJI_ENG.BIN -binary /FFEAFFFFEB00FFF7CF00FFECC63FFFE9FFE8FF/FFEAFFFFF7FFECC6FFFFEB05FFFFE8FFF7FFFFF9/  -yes
+
+# enable debug mode (press Start on 2nd pad to navigate event files) https://web.archive.org/web/20200918203538/https://github.com/cyberwarriorx/pcrown/wiki/Debugging
+#ucon64 --nbak --poke=1EB7F:01 0.BIN
+
+# add splashscreen -> crash some emus?
+#mv 0.BIN  0_org.BIN
+#wine mksplash.exe ../mksplash/splash_loader.bin ../mksplash/splash.bmp 0_org.BIN 0.BIN
 
 cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 0.BIN  0.BIN
 cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" KANJI.BIN  KANJI_ENG.BIN
 
 # update events
 if [ "$1" != "noscript" ]; then
-    7z e -y "Princess Crown (Japan) (1M) (Track 01) (English).iso" *.EVN
+    7z e -y "Princess Crown (Japan) (1M) (Track 01).iso" *.EVN
     # enforce correct line splitting
     for txt in  ${TRANSLATED_SCRIPT_PATH}/events/*.txt ; do
         echo "splitting text in $txt"
         python3 _split_long_lines.py "$txt"  ${TRANSLATED_SCRIPT_PATH}/events_splitted_35chars/$(basename $txt)
     done
-fi
-find *.EVN | while read eventfile; do 
-    EVNBASENAME="$(basename "$eventfile" .EVN )"
-    if [ -f ${TRANSLATED_SCRIPT_PATH}/events_splitted_35chars/${EVNBASENAME}.TXT ]; then
-        echo "updating ${eventfile}"
-        wine eventeditor.exe -i ${eventfile} ${TRANSLATED_SCRIPT_PATH}/events_splitted_35chars/${EVNBASENAME}.TXT  -o ${eventfile}.eng
-        #xdelta3 -e -s ${eventfile}  ${eventfile}.eng ${eventfile}.xdelta
-        cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" ${eventfile}  ${eventfile}.eng  > /dev/null
-    else
-        echo "missing translation script: ${TRANSLATED_SCRIPT_PATH}/events_splitted_35chars/${EVNBASENAME}.TXT"
-    fi
-done
 
-# temp. fix for dragon fight softlock (restore jap ver)  https://github.com/eadmaster/pcrown/issues/30
-cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 015_00_1.EVN 015_00_1.EVN
-# temp. fix for softlock at Larva boss (restore jap ver)  https://github.com/eadmaster/pcrown/issues/88
-cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 176_00_0.EVN 176_00_0.EVN
+    find *.EVN | while read eventfile; do 
+        EVNBASENAME="$(basename "$eventfile" .EVN )"
+        if [ -f ${TRANSLATED_SCRIPT_PATH}/events_splitted_35chars/${EVNBASENAME}.TXT ]; then
+            echo "updating ${eventfile}"
+            wine eventeditor.exe -i ${eventfile} ${TRANSLATED_SCRIPT_PATH}/events_splitted_35chars/${EVNBASENAME}.TXT  -o ${eventfile}.eng
+            #xdelta3 -e -s ${eventfile}  ${eventfile}.eng ${eventfile}.xdelta
+            cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" ${eventfile}  ${eventfile}.eng  > /dev/null
+        else
+            echo "missing translation script: ${TRANSLATED_SCRIPT_PATH}/events_splitted_35chars/${EVNBASENAME}.TXT"
+        fi
+    done
+
+    # temp. fix for dragon fight softlock (overwrite jap text)  https://github.com/eadmaster/pcrown/issues/30
+    xdelta3 -f -d -s 015_00_1.EVN 015_00_1.EVN.xdelta 015_00_1.EVN.fixed
+    cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 015_00_1.EVN 015_00_1.EVN.fixed
+    # temp. fix for softlock at Larva boss (overwrite jap text)  https://github.com/eadmaster/pcrown/issues/88
+    xdelta3 -f -d -s 176_00_0.EVN 176_00_0.EVN.xdelta 176_00_0.EVN.fixed
+    cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 176_00_0.EVN 176_00_0.EVN.fixed
+    # temp. fix for PEOPLE FULL bug in Cado Bado (add missing cmds)  https://github.com/eadmaster/pcrown/issues/71
+    xdelta3 -f -d -s 041_00_1.EVN 041_00_1.EVN.xdelta 041_00_1.EVN.fixed
+    cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 041_00_1.EVN 041_00_1.EVN.fixed
+    #TODO: temp. fix for softlock at dialog with the wizard turned into a frog () https://github.com/eadmaster/pcrown/issues/89
+    #cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 061_00_2.EVN  061_00_2.EVN.fixed
+fi
+
+# replace BEGIN text in load save dialog  https://github.com/eadmaster/pcrown/issues/90
+#7z e -y "Princess Crown (Japan) (1M) (Track 01).iso" COCKPIT.CHB
+#sfk replace COCKPIT.CHB -binary /$( xxd -p -c 10000 save_begin_jap.bin)/$( xxd -p -c 10000 save_begin_eng.bin)/  -yes    
 
 # doorway signs translation  https://github.com/eadmaster/pcrown/issues/5
-# TODO: replace with ucon64 --hreplace="S:R"
-7z e -y "Princess Crown (Japan) (1M) (Track 01) (English).iso" *.CHR
+7z e -y "Princess Crown (Japan) (1M) (Track 01).iso" *.CHR
+file2hexstr() { xxd -p -c 10000  "$1" | tr 1 F ; }
+replace_sign() {
+    FILEBASENAME=${SIGNS_PATH}/$1
+    CORRECT_FILESIZE=$2
+	if [ $(stat -c%s "${FILEBASENAME}_jap.bin") -ne $CORRECT_FILESIZE ]; then
+		echo "$0: sign ${FILEBASENAME}_jap.bin wrong size or does not exist (skipped)"
+		return
+	fi
+	if [ $(stat -c%s "${FILEBASENAME}_eng.bin") -ne $CORRECT_FILESIZE ]; then
+		echo "$0: sign ${FILEBASENAME}_eng.bin wrong size or does not exist (skipped)"
+		return
+	fi
+    
+    SIGN_JAP_HEX_STR=$(file2hexstr ${FILEBASENAME}_jap.bin)
+    SIGN_ENG_HEX_STR=$(file2hexstr ${FILEBASENAME}_eng.bin)
+    #echo $SIGN_JAP_HEX_STR
+    #echo $SIGN_ENG_HEX_STR
+    #return
+    sfk replace $chrfile -binary /$SIGN_JAP_HEX_STR/$SIGN_ENG_HEX_STR/  -yes    
+    # TODO? replace with ucon64 -nbak --hreplace="S:R"
+}
+
 find *.CHR | while read chrfile; do 
     # PUB signs (24*11/2 = 132 bytes)
-    sfk replace $chrfile -binary /0F00FFFFFFF000F00FFFFF0000F000F0F00000F00F000F00000000F0F00000F00FFFFF00F000FFFFFFF0FFFF0F000F000F00F0F0F0F000F0FFFFFFF00000F0F0F0F000F000F000000000FF00FFF000F00FFFFFF00F00F00000F000F0F0F0F0F00F00FFFFFFF0FF000F00F0F0F000F00000F00000F00F00F0F000FFFFFFF0000000F00F00/0FFFFF000F00000F0FFFFF000F0000F00F00000F00F000F00F00000F0F00000F00F0000F0F00000F0F00000F00F0000F0F00000F0F00000F00F000F00F0000F00F00000F00FFFF000FFFFF000F00000F00F000F00F0000000F00000F00F0000F0F0000000F00000F00F0000F0F00000000F000F000F000F00F000000000FFF000FFFFF00/  -yes
-    
+    replace_sign  pub  132
     # NPC signs (24*11/2)
-    sfk replace $chrfile -binary /0FFFFFFFFF0000000F0000000F0000000F00FFFFFFFFFFF00F0000000F00F000000000F00FFFFFFFFF0000FFFFFFF0000F0000F000000000F0F000000F0000F000000FFF0F0F0F000FFFFFFFFFF00000FFF0F0000F00000F00000FFF00F0F0000F00000F00000000FFF00F000F0FF000F0F00FFF00F000F0FFF000000FF00000FF000000/0F00000F0FFFFF00000FFF000FF0000F0F0000F000F000F00F0F000F0F00000F0F00000F0F0F000F0F00000F0F00000F0F00F00F0F00000F0F0000000F00F00F0F0000F00F0000000F000F0F0FFFFF000F0000000F000F0F0F0000000F00000F0F0000FF0F0000000F00000F0F0000FF0F00000000F000F00F00000F0F000000000FFF00/  -yes
-    
+    replace_sign  npc  132
     # shop sign (16*11/2 = 88 bytes)
-    # "$" sign (centered):
-    sfk replace $chrfile -binary /0FFFFFFFFF0000000F0000000F0000000FFFFFFFFF0000000F000000000000000FFFFFFFFFF000000F00F000F00000000F0FFFFF0F0000000F0000F0000000000F0FFFFFFF000000F00000F000000000F0FFFFFFFFF00000/0000000F0000000000000FFFFF0000000000F00F00F000000000F00F000000000000F00F0000000000000FFFFF0000000000000F00F000000000000F00F000000000F00F00F0000000000FFFFF0000000000000F00000000/  -yes
- 
-    # Med (shop) (16*11/2 = 88 bytes)
-    sfk replace $chrfile -binary /000F000F00000000FFFFFFFFFFF0000000000F0000000000F00FFFFF00F000000FFF000FFF0000000F0FFFFF0F000000F0000F0000F00000FFFFFFFFFFF000000000FFF00000000000FF0F0FF0000000FF000F000FF00000/F000F0FFFF0FFF00FF0FF0F00F0F00F0F0F0F0F00F0F000FF0F0F0F0000F000FF000F0F0000F000FF000F0FFF00F000FF000F0F0000F000FF000F0F0000F000FF000F0F00F0F000FF000F0F00F0F00F0F000F0FFFF0FFF00/  -yes
-
+    replace_sign  shop  88
+    # Med (shop) (16*11/2 = 88 bytes) -> test in 002-00
+    replace_sign  med_shop  88
     # Item (shop) (24*11/2 = 132 bytes)
-    sfk replace $chrfile -binary /0F0000F0F00000FFFFFFF00000F0FFFFFFF000F00000F0000000000F000000FFFFFFF00000000FFFFF0000F00000F000FFF00F000F0000FFFFFFF00000F00FFFFF0000F00000F00000F00F000F0000FFFFFFF00000F00F000F0000000000000000F00FFFFF00FFFFFFFFFFF00F0F0000000000F00000F000F000FFFFFFF00F0000000F00/FFF0FFFFF0FFFFFF0F00000F0F0000F0000F000F0FF000FF0F0000F0000F000F0F0F0F0F0F0000F0000F00000F00F00F0F0000F0000F00000F00F00F0F0000F0000FFF000F00000F0F0000F0000F00000F00000F0F0000F0000F00000F00000F0F0000F0000F000F0F00000F0F0000F0000F000F0F00000FFFF000F000FFFFFF0F00000F/  -yes
-    
+    replace_sign  item_shop  132
     # Inn (shop)(16*11/2 = 88 bytes)
-    sfk replace $chrfile -binary /00000F0000000000FFFFFFFFFFF00000F000000000F0000000F000000000000000F0FFFFFF0000000F0000F000000000FF00FFFFFF0000000F00F0000F0000000F00FFFFFF0000000F00F0000F0000000F00FFFFFF000000/FFFF0F000F0F000F0F000FF00F0FF00F0F000F0F0F0F0F0F0F000F0F0F0F0F0F0F000F00FF0F00FF0F000F00FF0F00FF0F000F000F0F000F0F000F000F0F000F0F000F000F0F000F0F000F000F0F000FFFFF0F000F0F000F/  -yes
-    
-    # BAKE (shop) (24*10/2=120)
-    sfk replace $chrfile -binary /0000000000F0000000000000000F000F0F0F00F000000000000F000F00F0000FF00000F000F00000F00000000F0000F000F00000F000000000000F0000F000000F00000000000F0000F000000F0000000000F0000F0000000F000000000F00000F00000000F000000FF00000F000000000F000FFF0000000/FFF00000FF000F000F0FFFFFF00F000F00F00F000F0F0000F000F00F00F00F00F00F0000F000F00F00F00F0F000F0000F00F00F0000F0FF0000F0000FFF000F0000F0F00000FFFF0F00F00F0000F0FF0000F0000F000F0FFFFFF0F0F000F0000F000F0F0000F0F00F00F0000FFFF00F0000F0F000F0FFFFF/  -yes
+    replace_sign  inn  88
+    # Castle  (96*11/2 = 528) -> test in 002-00
+    replace_sign  castle  528
+    # Baker (shop) (24*10/2=120) -> test in 004-00
+    replace_sign  baker_shop  120
+    # Magic shop (24*11/2=132) -> test in 045-00
+    replace_sign  magic_shop  132
+    # School (24*11/2=132) -> test in 045-00
+    replace_sign  school  132
+    # Warp/Teleportation Magic (24*11/2 = 132)  -> test in 045-00
+    replace_sign warp  132
+    # courtyard (inside castle) (24*11/2 = 132)-> test in 000-01
+    replace_sign  courtyard  132
+    # Room (inside castle) (24*11/2 = 132) -> test in 000-04
+    replace_sign  room  132
+    # Audience Hall (inside castle) (48*11/2 = 264) -> test in 000-03
+    replace_sign  aud_hall  264
+    # Floor (16*11/2 = 88) -> "F"
+    replace_sign  floor  88
+    # 2nd/3rd Floor (24*11/2 = 132) -> "3 F"
+    replace_sign  floor3  132
+    replace_sign  floor2  132
+    # "he" kana (16*5/2) -> cleared
+    replace_sign  he_kana  40
+    # "spell kanji (16*11/2 = 88) -> cleared
+    replace_sign  spell_kanji 88
+    # "no" kana (16*8/2=64) -> 's
+    replace_sign  no_kana  64
+    # Jestonai (72*12/2=432) -> test in 000-04 (sx)
+    replace_sign  jetsonai  432
+    # Kwein (48*10/2=240) -> test in 000-04 (sx)
+    replace_sign  kwein  240
+    # Sidrael (64*11/2=352) -> test in 000-04  (rx)
+    replace_sign  sidrael  352
+    # Eriel (48*10/2=240) -> test in 000-04  (rx)
+    replace_sign  eriel  240
+    # Gradriel (72*11/2=396) -> test in 000-04 (rx)
+    replace_sign  gradriel  396
         
     cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso"  $chrfile $chrfile
 done
 
-
 # build xdelta patch
-xdelta3 -f -e -s "Princess Crown (Japan) (1M) (Track 01) (English).iso.bak"  "Princess Crown (Japan) (1M) (Track 01) (English).iso"  "Princess.Crown.Japan.1M.Track.01.iso.xdelta"
+xdelta3 -S none -f -e -s "Princess Crown (Japan) (1M) (Track 01).iso"  "Princess Crown (Japan) (1M) (Track 01) (English).iso"  "Princess.Crown.Japan.1M.Track.01.iso.xdelta"
+
+# build xdelta patch (for original bin)
+#wine CDmage.exe "Princess Crown (Japan) (1M) (Track 01) (English).iso"  #  TODO: automate cdmage conversion
+#xdelta3 -S none -f -e -s "Princess Crown (Japan) (1M) (Track 01).bin"  "Princess Crown (Japan) (1M) (Track 01) (English).bin"  "Princess.Crown.Japan.1M.Track.01.bin.xdelta"
